f67f1a84eea6e1407fd43fdd4508a3625976d067
LU Yuyang <1155240112@link.cuhk.edu.hk>
2026-05-28 03:53:13 +0000

    0528 clear

--- backend/src/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require('express');
-const controller = require('../controllers/users.controller');
-
-const router = Router();
-
-router.get('/:id/stats', controller.getStats);
-
-module.exports = router;
--- frontend/android/app/src/main/kotlin/com/example/collection_journey_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.collection_journey_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
